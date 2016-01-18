85cd4d2e0335c1fe0dd5fbf15ed6e938ac41cc37
Igor Rybakov <igor@rybakov.kz>
2016-01-18 20:31:36 +0600

    parse command from json

--- src/main/scala/kz/rio/endpoint/ListenerActor.scala
+++ src/main/scala/kz/rio/endpoint/ListenerActor.scala
@@ -4,7 +4,10 @@ import akka.actor.{Props, ActorRef, ActorLogging, Actor}
 import com.github.sstone.amqp.Amqp._
 import com.github.sstone.amqp.{Amqp, Consumer, ConnectionOwner}
 import kz.rio.core.ServiceActor
+import kz.rio.domain.DomainMessage
+import org.json4s._
 import org.json4s.NoTypeHints
+import org.json4s.native.Serialization.{read, write}
 import org.json4s.native.JsonMethods._
 import org.json4s.native.Serialization
 
@@ -19,7 +22,7 @@ class ListenerActor(amqpConnection: ActorRef, responder: ActorRef) extends Actor
 
   import context._
 
-  implicit val formats = Serialization.formats(NoTypeHints)
+  implicit val formats = Serialization.formats(ShortTypeHints(List(classOf[ServiceActor.Ping],classOf[ServiceActor.Echo])))
 
   // create a consumer that will route incoming AMQP messages to our listener
   val queueParams = QueueParameters("request.queue", passive = false, durable = false, exclusive = false, autodelete = true)
@@ -34,17 +37,24 @@ class ListenerActor(amqpConnection: ActorRef, responder: ActorRef) extends Actor
   // tell our consumer to consume from it
   consumer ! AddQueue(queueParams)
 
+  def parseCommand(msgBody: String): DomainMessage = {
+    parse(msgBody).extract[DomainMessage]
+  }
+
   override def receive = {
     case Delivery(consumerTag, envelope, properties, body) => {
       val msgBody = new String(body)
       println("got a message: " + msgBody)
 
-      val ping = parse(msgBody).extract[ServiceActor.Ping]
+      val command = parseCommand(msgBody)
+
       val service = system.actorOf(ServiceActor.props(responder))
-      service ! ping
+      service ! command
 
       sender ! Ack(envelope.getDeliveryTag)
     }
   }
+
+
 
 }
